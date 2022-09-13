80fb6dfb56980a855bdcfcd4947a40cb119d5431
Carlos && Ponce <srtager555@gmail.com>
2022-09-12 21:06:48 -0600

    _document.js added

---
+++ src/pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>{/* Here I will put the fonts */}</Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
