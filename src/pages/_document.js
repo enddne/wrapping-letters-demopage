d02df764fce12f0c9275df1c7bc0816455f548d0
Carlos && Ponce <srtager555@gmail.com>
2022-09-16 03:07:10 -0600

    New fonts added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,22 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head>{/* Here I will put the fonts */}</Head>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin={true}
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Noto+Sans+Display:ital,wght@0,200;0,300;0,400;0,500;0,600;1,200&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:ital,wght@0,200;0,300;0,400;0,500;0,600;1,200&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
