cb188fe0a3497b92fbac5bb57c645fd5f3b07e74
Carlos && Ponce <srtager555@gmail.com>
2022-09-22 11:54:19 -0600

    first test with TS

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { WlSlider } from "@components/wl-slider.component";
+
+export default function home() {
+  return (
+    <>
+      <WlSlider />
+      <p>Hello?</p>
+    </>
+  );
+}
