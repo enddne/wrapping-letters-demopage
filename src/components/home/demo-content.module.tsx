0e29d94f923f040dd3da087f32afce9256a49d9f
Carlos && Ponce <srtager555@gmail.com>
2022-09-23 03:06:53 -0600

    I added the structure to the content

--- src/components/home/demo-content.module.tsx
+++ src/components/home/demo-content.module.tsx
@@ -1,5 +1,76 @@
+import Wl from "wrapping-letters-react";
+
 const styles = require("@styles/home/content.module.scss");
 
 export function DemoContent() {
-  return <div className="container"></div>;
+  const wordsToSearch: string[] = ["Wrapping", "Letters"];
+
+  const StrongTag = ({
+    letter,
+    cssClass,
+  }: {
+    letter: string;
+    cssClass: string;
+  }) => {
+    return <strong className={styles[cssClass]}>{letter}</strong>;
+  };
+
+  return (
+    <>
+      <section className={`${styles.textToLeft} ${styles["magin-bottom"]}`}>
+        <div className={styles.textContainer}>
+          <h2>What does Wrapping Letters do?</h2>
+          <p>
+            <Wl
+              text="Wrapping letters wrap for you the text in your code editor, simplify the task to animate in very few code lines"
+              textOptions={{
+                SpecialWrapp: {
+                  wordToSearch: wordsToSearch,
+                  structureToAdd: StrongTag,
+                },
+                PerWord: true,
+              }}
+            />
+          </p>
+        </div>
+      </section>
+      <section className={`${styles.textToRigth} ${styles["magin-bottom"]}`}>
+        <div className={styles.textContainer}>
+          <h2>How does Wrapping Letters do?</h2>
+          <p>
+            <Wl
+              text='Wrapping Letters search for the words through an iteration that returns a "list" of JSX elements that you can manipulate as you please'
+              textOptions={{
+                SpecialWrapp: {
+                  wordToSearch: wordsToSearch,
+                  structureToAdd: StrongTag,
+                },
+                PerWord: true,
+              }}
+            />
+          </p>
+        </div>
+      </section>
+      <section className={`${styles.textToLeft} ${styles["magin-bottom"]}`}>
+        <div className={styles.textContainer}>
+          <h2>Code difference</h2>
+          <p>
+            <Wl
+              text="Here an example of the difference with natural code vs. Wrapping Letters"
+              textOptions={{
+                SpecialWrapp: {
+                  wordToSearch: wordsToSearch,
+                  structureToAdd: StrongTag,
+                },
+                PerWord: true,
+              }}
+            />
+          </p>
+          <video>
+            <p>A problem hehehe...</p>
+          </video>
+        </div>
+      </section>
+    </>
+  );
 }
