1b39e839404ce29ff0e330477dc4f8ea55ec4630
Carlos && Ponce <srtager555@gmail.com>
2022-09-22 23:32:11 -0600

    hydratation bug fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
+import { useState, useEffect } from "react";
+
 import "@styles/global.css";
 
 export default function HyperDemoPage({ Component, pageProps }) {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
+  if (!loaded) return;
+
   return <Component {...pageProps} />;
 }
