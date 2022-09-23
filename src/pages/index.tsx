9fc87b879d4b2bd60ff2dd2561d7dbc3102bd0a3
Carlos && Ponce <srtager555@gmail.com>
2022-09-23 03:03:48 -0600

    I added the home index styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,16 @@ import { DemoContent } from "@components/home/demo-content.module";
 import { Intrduction } from "@components/home/introduction.component";
 import { WlSlider } from "@components/home/wl-slider.component";
 
+const styles = require("@styles/home/index.module.scss");
+
 export default function home() {
   return (
     <>
       <WlSlider />
-      <header className="container">
+      <header className={`container ${styles.header}`}>
         <Intrduction />
       </header>
-      <main className="container">
+      <main className={`container ${styles.main}`}>
         <DemoContent />
       </main>
     </>
