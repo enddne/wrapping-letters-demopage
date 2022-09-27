cd64f4625dac8b760da8ab660682843680a6fa0f
Carlos && Ponce <srtager555@gmail.com>
2022-09-26 18:06:17 -0600

    I upgraded the JSX to add the new component for animations

--- src/components/home/demo-content.module.tsx
+++ src/components/home/demo-content.module.tsx
@@ -1,5 +1,5 @@
-import Wl from "wrapping-letters-react";
-import { TextAppear } from "@common/appearTextAnimation";
+import WL from "wrapping-letters-react";
+import { ContentAppear } from "@common/appearContentAnimation";
 
 const styles = require("@styles/home/content.module.scss");
 
@@ -20,56 +20,70 @@ export function DemoContent() {
     <>
       <section className={`${styles.textToLeft} ${styles["magin-bottom"]}`}>
         <div className={styles.textContainer}>
-          <h2>What does Wrapping Letters do?</h2>
-          <p>
-            <TextAppear
-              text="Wrapping Letters wrap for you the text in your code editor, simplify the task to animate in very few code lines"
-              options={{
-                SpecialWrapp: {
-                  wordToSearch: wordsToSearch,
-                  structureToAdd: StrongTag,
-                },
-                PerWord: true,
-              }}
-            />
-          </p>
+          <ContentAppear>
+            <h2>What does Wrapping Letters do?</h2>
+          </ContentAppear>
+          <ContentAppear>
+            <p>
+              <WL
+                text="Wrapping Letters wrap for you the text in your code editor, simplify the task to animate in very few code lines"
+                textOptions={{
+                  SpecialWrapp: {
+                    wordToSearch: wordsToSearch,
+                    structureToAdd: StrongTag,
+                  },
+                  PerWord: true,
+                }}
+              />
+            </p>
+          </ContentAppear>
         </div>
       </section>
       <section className={`${styles.textToRigth} ${styles["magin-bottom"]}`}>
         <div className={styles.textContainer}>
-          <h2>How does Wrapping Letters do?</h2>
-          <p>
-            <TextAppear
-              text='Wrapping Letters search for the words through an iteration that returns a "list" of JSX elements that you can manipulate as you please'
-              options={{
-                SpecialWrapp: {
-                  wordToSearch: wordsToSearch,
-                  structureToAdd: StrongTag,
-                },
-                PerWord: true,
-              }}
-            />
-          </p>
+          <ContentAppear>
+            <h2>How does Wrapping Letters do?</h2>
+          </ContentAppear>
+          <ContentAppear>
+            <p>
+              <WL
+                text='Wrapping Letters search for the words through an iteration that returns a "list" of JSX elements that you can manipulate as you please'
+                textOptions={{
+                  SpecialWrapp: {
+                    wordToSearch: wordsToSearch,
+                    structureToAdd: StrongTag,
+                  },
+                  PerWord: true,
+                }}
+              />
+            </p>
+          </ContentAppear>
         </div>
       </section>
       <section className={`${styles.textToLeft} ${styles["magin-bottom"]}`}>
         <div className={styles.textContainer}>
-          <h2>Code difference</h2>
-          <p>
-            <TextAppear
-              text="Here an example of the difference with natural code vs. Wrapping Letters"
-              options={{
-                SpecialWrapp: {
-                  wordToSearch: wordsToSearch,
-                  structureToAdd: StrongTag,
-                },
-                PerWord: true,
-              }}
-            />
-          </p>
-          <video>
-            <p>A problem hehehe...</p>
-          </video>
+          <ContentAppear>
+            <h2>Code difference</h2>
+          </ContentAppear>
+          <ContentAppear>
+            <p>
+              <WL
+                text="Here an example of the difference with natural code vs. Wrapping Letters"
+                textOptions={{
+                  SpecialWrapp: {
+                    wordToSearch: wordsToSearch,
+                    structureToAdd: StrongTag,
+                  },
+                  PerWord: true,
+                }}
+              />
+            </p>
+          </ContentAppear>
+          <ContentAppear>
+            <video>
+              <p>A problem hehehe...</p>
+            </video>
+          </ContentAppear>
         </div>
       </section>
     </>
