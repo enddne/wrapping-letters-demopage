7ec5bdb884300476bf95e696ffae89c8fd343436
Carlos && Ponce <srtager555@gmail.com>
2022-09-28 02:50:36 -0600

    the base to the content is ready

---
+++ src/DDBB/pageData.ddbb.ts
@@ -0,0 +1,23 @@
+import { ID, WL } from "@common/type";
+
+type Content = {
+  id: string;
+  content: {
+    tag: "h2" | "a" | "p" | "img" | "h3" | "note" | "code";
+    text: string;
+    wlProps?: WL;
+  }[];
+};
+
+export const PageData: Record<ID, Content> = {
+  "0000": {
+    id: "0000",
+    content: [
+      { tag: "h2", text: "first thing's first" },
+      {
+        tag: "p",
+        text: "You do can install wrapping letter in your projects with next comand line in your terminal",
+      },
+    ],
+  },
+};
