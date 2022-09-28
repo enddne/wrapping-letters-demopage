4374e4f151f30d39787214ec95a1bc0eeb405368
Carlos && Ponce <srtager555@gmail.com>
2022-09-28 03:25:03 -0600

    new attribute added

--- src/DDBB/headerData.ddbb.ts
+++ src/DDBB/headerData.ddbb.ts
@@ -2,6 +2,7 @@ import { ID } from "@common/type";
 
 type PageContent = {
   id: ID;
+  url: string;
   title: string;
   description: string;
 };
@@ -9,6 +10,7 @@ type PageContent = {
 export const Data: Record<ID, PageContent> = {
   "0000": {
     id: "0000",
+    url: "getting_started",
     title: "Getting Started",
     description: "Here you'll learn how to use Wrapping Letters",
   },
