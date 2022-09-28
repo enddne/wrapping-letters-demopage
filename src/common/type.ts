872e05697b6f032ffbc97f18a9f0a21d3c87a8dc
Carlos && Ponce <srtager555@gmail.com>
2022-09-28 02:47:38 -0600

    I created a file with each "type" to export

---
+++ src/common/type.ts
@@ -0,0 +1,18 @@
+export type ID = string;
+
+export type SpecialArray = string | string[];
+
+export type WL = {
+  ClassToAdd?: string;
+  SelectClass?: {
+    wordToSearch: SpecialArray;
+    classToAdd: SpecialArray;
+    spaceBeetwenWord: boolean;
+  };
+  SpecialWrapp: {
+    wordToSearch: SpecialArray;
+    strucutureToAdd: SpecialArray;
+  };
+  PerWord: boolean;
+  strucuture: Function;
+};
