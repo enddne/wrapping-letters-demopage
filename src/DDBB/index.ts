8dda6ca51a960a4023c7db31ff5f27977c8cac10
Carlos && Ponce <srtager555@gmail.com>
2022-09-29 02:21:21 -0600

    I created a new method to the manager

--- src/DDBB/index.ts
+++ src/DDBB/index.ts
@@ -1,7 +1,14 @@
-export class DataManager {
-  id: string;
+import { ID, PageContent } from "@common/type";
 
-  constructor(id: string) {
-    this.id = id;
+export class DataManager {
+  constructor() {}
+
+  async getHeaderByID(
+    headers: Record<ID, PageContent>,
+    id: string
+  ): Promise<PageContent | null> {
+    if (!Object.prototype.hasOwnProperty.call(headers, id)) return null;
+
+    return headers[id];
   }
 }
