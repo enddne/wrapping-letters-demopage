58f65a8a1e4593b74c0cd1ceaedf00687774261c
Carlos && Ponce <srtager555@gmail.com>
2022-10-01 17:50:53 -0600

    I created a template to the docs

--- src/templates/docs/index.tsx
+++ src/templates/docs/index.tsx
@@ -1,1 +1,32 @@
-// xd
+import { Content, ID, PageContent } from "@common/type";
+import { GetStaticProps } from "next";
+import styles from "@styles/docs/index.module.scss";
+import { Aside } from "@components/document/sidebar.component";
+
+export function DocumentationTemplate({
+  header,
+  content,
+  allHeaders,
+}: {
+  header: PageContent;
+  content: Content;
+  allHeaders?: Record<ID, PageContent>;
+}) {
+  return (
+    <div className={styles.container}>
+      <Aside currentHeader={header} allHeaders={allHeaders} />
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allHeaders = new Promise((resolve, rejects) => {
+    const DATA = fetch(`${process.env.API}/docs/`)
+      .then((data) => data.json())
+      .catch(() => rejects("An error in the fetch, internal server error"));
+
+    resolve(DATA);
+  });
+
+  return { props: { allHeaders } };
+};
