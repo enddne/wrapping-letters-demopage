a7f2a8229f571970718ced1643d63d88474bda45
Carlos && Ponce <srtager555@gmail.com>
2022-10-01 03:40:14 -0600

    Test to solved the fetch failed in production

--- src/pages/docs/[id].tsx
+++ src/pages/docs/[id].tsx
@@ -4,26 +4,26 @@ import { GetStaticPaths, GetStaticProps } from "next";
 export default function SearchDocs({ data }: { data: PageContent }) {
   return (
     <>
-      <span>{data.title}</span> | <span>{data.description}</span>
+      <span>{data?.title}</span> | <span>{data?.description}</span>
     </>
   );
 }
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const res = await fetch(`${process.env.API}/docs`);
-  const posts: Record<ID, PageContent> = await res.json();
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const res = await fetch(`${process.env.API}/docs`);
+//   const posts: Record<ID, PageContent> = await res.json();
 
-  const paths = Object.values(posts).map((post: PageContent) => ({
-    params: { id: post.id },
-  }));
+//   const paths = Object.values(posts).map((post: PageContent) => ({
+//     params: { id: post.id },
+//   }));
 
-  return { paths, fallback: false };
-};
+//   return { paths, fallback: false };
+// };
 
-export const getStaticProps: GetStaticProps = async (context) => {
-  const res = await fetch(`${process.env.API}/docs/${context.params?.id}`);
+// export const getStaticProps: GetStaticProps = async (context) => {
+//   const res = await fetch(`${process.env.API}/docs/${context.params?.id}`);
 
-  const data = await res.json();
+//   const data = await res.json();
 
-  return { props: { data } };
-};
+//   return { props: { data } };
+// };
