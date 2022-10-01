993a9bdcaf1ed0776e1fb4c9b3c0cbbfdcafba99
Carlos && Ponce <srtager555@gmail.com>
2022-10-01 17:50:09 -0600

    I did pull apart the sections with a new 2 objects

--- src/DDBB/headerData.ddbb.ts
+++ src/DDBB/headerData.ddbb.ts
@@ -1,9 +1,18 @@
 import { ID, PageContent } from "@common/type";
 
-export const Data: Record<ID, PageContent> = {
-  "getting-started": {
-    id: "getting-started",
-    title: "Getting Started",
-    description: "Here you'll learn how to use Wrapping Letters",
+export const Data: Record<ID, Record<ID, PageContent>> = {
+  "Getting started": {
+    "getting-started": {
+      id: "getting-started",
+      title: "Getting Started",
+      description: "Here you'll learn how to use Wrapping Letters",
+    },
+  },
+  "Component Syntaxis": {
+    text: {
+      id: "text",
+      title: "The most impotant props: text<string>",
+      description: "Here you'll learn how to use Wrapping Letters",
+    },
   },
 };
