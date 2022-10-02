2548b4f4dd37ef3b1ba5e0074cfb07edbf0afb47
Carlos && Ponce <srtager555@gmail.com>
2022-10-01 22:50:02 -0600

    test to I can get the data

--- src/pages/docs/index.tsx
+++ src/pages/docs/index.tsx
@@ -1,3 +1,18 @@
-export default function GettingStarted() {
-  return "A";
+import { GetStaticProps } from "next";
+
+export default function GettingStarted({ DATA }) {
+  console.log(
+    Object.values(DATA)
+      .map((e) => Object.values(e))
+      .flat()
+  );
+  return "a";
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const DATA = await fetch(`${process.env.API}/docs`).then((data) =>
+    data.json()
+  );
+
+  return { props: { DATA } };
+};
