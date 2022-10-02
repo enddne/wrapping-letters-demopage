50042bee608d9929a2ffff345dc3cec6403b33b6
Carlos && Ponce <srtager555@gmail.com>
2022-10-02 00:06:30 -0600

    issue when the API doesn't find the data, fixed

--- src/pages/api/docs/[id].ts
+++ src/pages/api/docs/[id].ts
@@ -1,8 +1,16 @@
 import { Data } from "@DDBB/headerData.ddbb";
 import { NextApiRequest, NextApiResponse } from "next";
 import { DynamicApiRoute } from "@modules/dynamicApi.module";
+import { PageContent } from "@common/type";
 
-const API = async (req: NextApiRequest, res: NextApiResponse) =>
-  DynamicApiRoute(req, res, Data);
+const API = async (req: NextApiRequest, res: NextApiResponse) => {
+  let values = Object.values(Data)
+    .map((e) => Object.entries(e))
+    .flat();
+
+  let DATA: Record<string, PageContent> = Object.fromEntries(values);
+
+  return DynamicApiRoute(req, res, DATA);
+};
 
 export default API;
