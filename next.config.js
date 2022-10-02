193fcb75c66054065f1f1ce72706828eba5df104
Carlos && Ponce <srtager555@gmail.com>
2022-10-02 01:06:28 -0600

    first redirect created

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/getting-started",
+        permanent: true,
+      },
+    ];
+  },
 };
