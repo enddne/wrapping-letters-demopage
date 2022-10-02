2dd0b6c1314c2a4268d6ecca619b00ffb614f155
Carlos && Ponce <srtager555@gmail.com>
2022-10-01 22:52:09 -0600

    I solved the problem about undefiend param

--- src/pages/docs/[id].tsx
+++ src/pages/docs/[id].tsx
@@ -1,47 +1,33 @@
-import { ID, PageContent, Content } from "@common/type";
+import { ID, PageContent } from "@common/type";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { DocumentationTemplate } from "templates/docs";
 
 export default function SearchDocs({
-  data,
+  HEADER_JSON,
 }: {
-  data: { HEADER_JSON: PageContent; CONTENT_JSON: Content };
+  HEADER_JSON: PageContent;
 }) {
-  return (
-    <DocumentationTemplate
-      header={data.HEADER_JSON}
-      content={data.CONTENT_JSON}
-    />
-  );
+  return <DocumentationTemplate header={HEADER_JSON} />;
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const res = await fetch(`${process.env.API}/docs`);
-  const posts: Record<ID, PageContent> = await res.json();
+  const posts: Record<ID, Record<ID, PageContent>> = await res.json();
 
-  const paths = Object.values(posts).map((post: PageContent) => ({
-    params: { id: post.id },
-  }));
+  const paths = Object.values(posts)
+    .map((e) => Object.values(e))
+    .flat()
+    .map((post: PageContent) => ({
+      params: { id: post.id },
+    }));
 
   return { paths, fallback: false };
 };
 
-export const getStaticProps: GetStaticProps = async (context) => {
-  const HEADER_JSON = new Promise<PageContent>((resolve, reject) => {
-    const DATA = fetch(`${process.env.API}/docs/${context.params?.id}`)
-      .then((data) => data.json())
-      .catch(() => reject("An error in the fetch, internal server error"));
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const HEADER_JSON = await fetch(`${process.env.API}/docs/${params?.id}`).then(
+    (data) => data.json()
+  );
 
-    resolve(DATA);
-  });
-
-  const CONTENT_JSON = new Promise<Content>((resolve, reject) => {
-    const DATA = fetch(`${process.env.API}/docs/content/${context.params?.id}`)
-      .then((data) => data.json())
-      .catch(() => reject("An error in the fetch, internal server error"));
-
-    resolve(DATA);
-  });
-
-  return { props: { data: { HEADER_JSON, CONTENT_JSON } } };
+  return { props: { HEADER_JSON } };
 };
