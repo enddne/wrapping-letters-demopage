73652852d10b7166cdfca6d54ef28693e9302c0b
Carlos && Ponce <srtager555@gmail.com>
2022-10-01 22:51:45 -0600

    I can't use Promise in fetching functions :c

--- src/templates/docs/index.tsx
+++ src/templates/docs/index.tsx
@@ -1,16 +1,16 @@
 import { Content, ID, PageContent } from "@common/type";
-import { GetStaticProps } from "next";
+import { GetStaticProps, GetStaticPaths } from "next";
 import styles from "@styles/docs/index.module.scss";
 import { Aside } from "@components/document/sidebar.component";
 
 export function DocumentationTemplate({
   header,
-  content,
   allHeaders,
+  CONTENT_JSON,
 }: {
   header: PageContent;
-  content: Content;
   allHeaders?: Record<ID, PageContent>;
+  CONTENT_JSON?: Content;
 }) {
   return (
     <div className={styles.container}>
@@ -19,14 +19,25 @@ export function DocumentationTemplate({
   );
 }
 
-export const getStaticProps: GetStaticProps = async () => {
-  const allHeaders = new Promise((resolve, rejects) => {
-    const DATA = fetch(`${process.env.API}/docs/`)
-      .then((data) => data.json())
-      .catch(() => rejects("An error in the fetch, internal server error"));
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res = await fetch(`${process.env.API}/docs/content`);
+  const posts: Record<ID, Content> = await res.json();
 
-    resolve(DATA);
-  });
+  const paths = Object.values(posts).map((post: Content) => ({
+    params: { id: post.id },
+  }));
 
-  return { props: { allHeaders } };
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const allHeaders = fetch(`${process.env.API}/docs/`).then((data) =>
+    data.json()
+  );
+
+  const CONTENT_JSON = fetch(
+    `${process.env.API}/docs/content/${context.params?.id}`
+  ).then((data) => data.json());
+
+  return { props: { allHeaders, CONTENT_JSON } };
 };
