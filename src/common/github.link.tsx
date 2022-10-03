c37e2c99680c379df4a7bb9fe905a2210679bc26
Carlos && Ponce <srtager555@gmail.com>
2022-10-03 00:30:13 -0600

    I created a component to Github anchor

---
+++ src/common/github.link.tsx
@@ -0,0 +1,12 @@
+import { BsGithub } from "react-icons/bs";
+
+export const GithubAnchor = () => (
+  <a
+    href="https://github.com/srtager555/wrapping-letters"
+    target="_blank"
+    rel="noreferrer"
+    className="github--anchor"
+  >
+    <BsGithub />
+  </a>
+);
