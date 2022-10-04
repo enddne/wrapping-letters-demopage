1ddf4f6a4510af1ca66d4875202a7c284b1942bc
Carlos && Ponce <srtager555@gmail.com>
2022-10-03 21:25:52 -0600

    I started the menu logic to mobile version

--- src/components/document/sidebar.component.tsx
+++ src/components/document/sidebar.component.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { ID, PageContent } from "@common/type";
 import { GithubAnchor } from "@common/github.link";
 import Link from "next/link";
@@ -11,27 +12,43 @@ export function Aside({
   currentHeader: PageContent;
   allHeaders: Record<ID, Record<ID, PageContent>>;
 }) {
+  let [isMobile, setIsMobile] = useState(true);
   let entriesArray = Object.entries(allHeaders);
 
+  useEffect(function () {
+    let resizing = () => {
+      if (window.innerWidth < 1024) setIsMobile(true);
+      else setIsMobile(false);
+    };
+
+    window.addEventListener("resize", resizing);
+    return () => {
+      window.removeEventListener("resize", resizing);
+    };
+  }, []);
+
   return (
-    <aside className={styles.container}>
-      <div className={styles.content}>
-        {entriesArray.map((el, index) => (
-          <div className={styles.stack} key={`${el[0]} ${index}`}>
-            <span className={styles["stack-title"]}>{el[0]}</span>
-            <div className={styles["stack--items__container"]}>
-              {Object.values(el[1]).map((element: PageContent) => (
-                <Link href={`/docs/${element.id}`} key={element.id}>
-                  <a>{element.title}</a>
-                </Link>
-              ))}
+    <>
+      {isMobile && <button className={styles["aside-button"]}>xd</button>}
+      <aside className={styles.container}>
+        <div className={styles.content}>
+          {entriesArray.map((el, index) => (
+            <div className={styles.stack} key={`${el[0]} ${index}`}>
+              <span className={styles["stack-title"]}>{el[0]}</span>
+              <div className={styles["stack--items__container"]}>
+                {Object.values(el[1]).map((element: PageContent) => (
+                  <Link href={`/docs/${element.id}`} key={element.id}>
+                    <a>{element.title}</a>
+                  </Link>
+                ))}
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-      <footer className={styles.footer}>
-        <GithubAnchor />
-      </footer>
-    </aside>
+          ))}
+        </div>
+        <footer className={styles.footer}>
+          <GithubAnchor />
+        </footer>
+      </aside>
+    </>
   );
 }
