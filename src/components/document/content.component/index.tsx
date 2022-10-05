1f3b40c24c6d2b7d14b5af36fc39dba523b12cf6
Carlos && Ponce <srtager555@gmail.com>
2022-10-05 01:24:13 -0600

    I created the JSX base to the content

---
+++ src/components/document/content.component/index.tsx
@@ -0,0 +1,33 @@
+import { Content, PageContent } from "@common/type";
+import { NotFound } from "pages/404";
+
+import styles from "@styles/docs/content.module.scss";
+
+export function Main({
+  currentHeader,
+  content,
+}: {
+  currentHeader: PageContent;
+  content?: Content;
+}) {
+  if (!content) return <NotFound />;
+
+  return (
+    <main className={styles.main}>
+      <div className={styles["content-container"]}>
+        <header className={styles.header}>
+          <h2>xd</h2>
+          {currentHeader.toString()}
+        </header>
+        <section className={styles.content}>
+          <h1>xd</h1>
+          {Object.entries(content).toString()}
+        </section>
+        <nav className={styles.navigator}>
+          <article>prev</article>
+          <article>next</article>
+        </nav>
+      </div>
+    </main>
+  );
+}
