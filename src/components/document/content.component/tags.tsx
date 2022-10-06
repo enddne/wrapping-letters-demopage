0d66ab1ace94e9626423b60352150995ccdb6d1b
Carlos && Ponce <srtager555@gmail.com>
2022-10-06 03:27:56 -0600

    I started the tag manager to the content

--- src/components/document/content.component/tags.tsx
+++ src/components/document/content.component/tags.tsx
@@ -1,1 +1,37 @@
-export default function TagManager(content: Content) {}
+import { ContentAppear } from "@common/appearContentAnimation";
+import Wl from "wrapping-letters-react";
+
+export default function TagManager({ content }: Content) {
+  const Elements: Record<Tags, ContentTextComponent> = {
+    h2: H2Element,
+    a: AnchorElement,
+    p: ParaElement,
+    img: any,
+  };
+
+  return content.map((el, index) => (
+    <div key={index}>
+      <ContentAppear>{Elements[el.tag]}</ContentAppear>
+    </div>
+  ));
+}
+
+const H2Element: ContentTextComponent = ({ data, options }) => {
+  return (
+    <h1>
+      <Wl text={data} textOptions={options && options} />
+    </h1>
+  );
+};
+
+const AnchorElement: ContentTextComponent = ({ data, options }) => (
+  <a href={data.url}>
+    <Wl text={data.text} textOptions={options && options} />
+  </a>
+);
+
+const ParaElement: ContentTextComponent = ({ data, options }) => (
+  <p>
+    <Wl text={data.text} textOptions={options && options} />
+  </p>
+);
