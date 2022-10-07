362efe44fe7eeb4e4ccc67367aebe11917079350
Carlos && Ponce <srtager555@gmail.com>
2022-10-06 19:16:38 -0600

    I upgraded the API syntax

--- src/DDBB/pageData.ddbb.ts
+++ src/DDBB/pageData.ddbb.ts
@@ -1,18 +1,18 @@
-import { ID, Content } from "@common/type";
-
 export const PageData: Record<ID, Content> = {
   "getting-started": {
     id: "getting-started",
     content: [
-      { tag: "h2", text: "first thing's first" },
+      { tag: "h2", data: { text: "first thing's first" } },
       {
         tag: "p",
-        text: "You do can install wrapping letter in your projects with next comand line in your terminal",
+        data: {
+          text: "You do can install wrapping letter in your projects with next comand line in your terminal",
+        },
       },
     ],
   },
   "best-pratice": {
     id: "best-pratice",
-    content: [{ tag: "h2", text: "Best Pratice" }],
+    content: [{ tag: "h2", data: { text: "Best Pratice" } }],
   },
 };
