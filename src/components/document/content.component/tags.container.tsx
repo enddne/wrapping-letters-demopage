f7dfc4e670c9ae3d7b2f2f2e27cbbb5db987e327
Carlos && Ponce <srtager555@gmail.com>
2022-10-06 19:51:42 -0600

    I moved the tags to the new file

---
+++ src/components/document/content.component/tags.container.tsx
@@ -0,0 +1,69 @@
+import Image from "next/image";
+import Link from "next/link";
+import Wl from "wrapping-letters-react";
+
+import styles from "@styles/docs/content.module.scss";
+
+export const H2Element: ContentTextComponent = ({ text = "", options }) => {
+  return (
+    <h2>
+      <Wl text={text} textOptions={options && options} />
+    </h2>
+  );
+};
+
+export const H3Element: ContentTextComponent = ({ text = "", options }) => {
+  return (
+    <h3>
+      <Wl text={text} textOptions={options && options} />
+    </h3>
+  );
+};
+
+export const ParaElement: ContentTextComponent = ({ text = "", options }) => (
+  <p>
+    <Wl text={text} textOptions={options && options} />
+  </p>
+);
+
+export const NoteElement: ContentTextComponent = ({ text }) => (
+  <div className={styles["note--container"]}>
+    <p>{text}</p>
+  </div>
+);
+export const CodeElement: ContentTextComponent = ({ text }) => (
+  <pre className={styles["note--container"]}>
+    <code>{text}</code>
+  </pre>
+);
+
+export const AnchorElement: ContentTextComponent = ({
+  anchor = {},
+  options,
+}) => {
+  if (anchor.target != "") {
+    return (
+      <Link href={anchor.url ? anchor.url : ""}>
+        <a>
+          <Wl text={anchor.text} textOptions={options && options} />
+        </a>
+      </Link>
+    );
+  }
+
+  return (
+    <a href={anchor.url} target={anchor.target} rel="noreferrer">
+      <Wl text={anchor.text} textOptions={options && options} />
+    </a>
+  );
+};
+
+export const ImageElement: ContentTextComponent = ({ image }) => {
+  const src = image?.src ? image.src : "";
+
+  return (
+    <div className={styles["image--box"]}>
+      <Image src={src} alt={image?.alt} />
+    </div>
+  );
+};
