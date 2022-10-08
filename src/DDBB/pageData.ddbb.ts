6a3e23591a199b8c739ed6ab7ed7c42cd82061a2
Carlos && Ponce <srtager555@gmail.com>
2022-10-08 04:02:04 -0600

    I started to create the styles to the text

--- src/DDBB/pageData.ddbb.ts
+++ src/DDBB/pageData.ddbb.ts
@@ -1,8 +1,23 @@
+import styles from "@styles/docs/content.module.scss";
+
 export const PageData: Record<ID, Content> = {
   "getting-started": {
     id: "getting-started",
     content: [
       { tag: "h2", data: { text: "first thing's first" } },
+      {
+        tag: "p",
+        data: {
+          text: "You do can install wrapping letter in your projects with next comand line in your terminal",
+          options: {
+            SelectClass: {
+              classToAdd: styles["simple-code"],
+              wordToSearch: "install",
+            },
+            PerWord: true,
+          },
+        },
+      },
       {
         tag: "p",
         data: {
