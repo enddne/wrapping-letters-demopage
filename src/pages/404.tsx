578015f0bbb689fddf35a893501af1f3590db4a7
Carlos && Ponce <srtager555@gmail.com>
2022-11-10 03:09:39 -0600

    new content to the 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,16 @@
+import Link from "next/link";
+
 export default function NotFound() {
   // thsi comment is to refresh the build and add the new content for while I create a real DDBB
-  return <span>404</span>;
+  return (
+    <>
+      <span>404</span>
+      <p>
+        <Link href={`${process.env.URL}/docs/`}>
+          <a>return to the docs</a>
+        </Link>
+      </p>
+      <small>Ignore this page, the budget is not enough :c</small>
+    </>
+  );
 }
