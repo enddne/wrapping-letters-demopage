1cc090b8fb33e2ae6158c5f9e1341346a58bb545
Carlos && Ponce <srtager555@gmail.com>
2022-11-20 18:18:19 -0600

    Refresh the build, again

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 
 export default function NotFound() {
   // thsi comment is to refresh the build and add the new content for while I create a real DDBB
+  // This is the second comment to refresh the build in vercel xd, well this is weird
   return (
     <>
       <span>404</span>
